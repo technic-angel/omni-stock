83276b9e24997b7d73904314d08c4f7dbd5be6c3
Melissa Berumen <technic.angel27@gmail.com>
2025-11-10 18:59:25 -0800

    feat(collectibles): split models into package (Vendor, UserProfile, CardDetails, Collectible) and add demo data loader

---
+++ core_api/collectibles/models/__init__.py
@@ -0,0 +1,11 @@
+from .collectible import Collectible
+from .vendor import Vendor
+from .user_profile import UserProfile
+from .card_details import CardDetails
+
+__all__ = [
+    "Collectible",
+    "Vendor",
+    "UserProfile",
+    "CardDetails",
+]
