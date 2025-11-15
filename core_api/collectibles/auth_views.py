f87e9c9a7f8506ce6e6a73c179bfbca3c57caa85
Melissa Berumen <technic.angel27@gmail.com>
2025-11-15 12:58:31 -0800

    feat(auth): make RegisterView GenericAPIView so OpenAPI generator can infer serializer; update OpenAPI baseline

--- core_api/collectibles/auth_views.py
+++ core_api/collectibles/auth_views.py
@@ -1,15 +1,16 @@
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
 
 
-class RegisterView(APIView):
+class RegisterView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = RegisterSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({'username': user.username, 'id': user.id}, status=status.HTTP_201_CREATED)
