512974a0964d2b01da5fb3c6a9fc699daf7453ef
Melissa Berumen <technic.angel27@gmail.com>
2025-11-21 18:37:44 -0800

    chore(frontend): expand collectible type to include sku/quantity/image

--- frontend/src/shared/types/collectibles.ts
+++ frontend/src/shared/types/collectibles.ts
@@ -7,6 +7,8 @@ export interface CardDetails {
 export interface Collectible {
   id: number
   name: string
+  sku?: string
+  quantity?: number
   language?: string
   market_region?: string
   image_url?: string | null
