2fd845841f25a97d38eccb97e6e1abdc5098bbc4
Melissa Berumen <technic.angel27@gmail.com>
2025-12-03 15:57:26 -0800

    fix: satisfy isort ordering

--- backend/inventory/tests/serializers/test_collectible_serializer.py
+++ backend/inventory/tests/serializers/test_collectible_serializer.py
@@ -1,8 +1,7 @@
 from decimal import Decimal
 
-import pytest
-
 from django.test import override_settings
+import pytest
 
 from backend.inventory.api.serializers import CollectibleSerializer
 from backend.inventory.models import Collectible
