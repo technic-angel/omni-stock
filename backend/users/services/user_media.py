10a328aa1197285fd5a7443b157ad13f63f10d0c
Melissa Berumen <technic.angel27@gmail.com>
2025-12-08 19:02:17 -0800

    style: appease isort in user media service

--- backend/users/services/user_media.py
+++ backend/users/services/user_media.py
@@ -2,7 +2,7 @@
 
 from django.db import transaction
 
-from backend.users.models import UserMedia, UserMediaType
+from backend.users.models import UserMediaType, UserMedia
 
 @transaction.atomic
 def upsert_user_media(*, user_id: int, media_type: str, payload: dict) -> UserMedia:
