64690dda58839dab07937d219f373b3e509ee6ac
Melissa Berumen <technic.angel27@gmail.com>
2026-02-02 17:27:54 -0800

    style: run isort + black to fix import formatting

--- backend/inventory/api/views.py
+++ backend/inventory/api/views.py
@@ -5,8 +5,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from backend.inventory.selectors.overview import get_inventory_overview
 from backend.inventory.api.serializers import InventoryOverviewSerializer
+from backend.inventory.selectors.overview import get_inventory_overview
 
 
 class InventoryOverviewView(APIView):
